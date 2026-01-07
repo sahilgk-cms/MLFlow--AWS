be4768be79fec24632a47f7cadc9071dfd520d34
sahilgk-cms <sahil@catalysts.org>
2026-01-07 16:56:51 +0530

    data retrieve

---
+++ data_retrieve.py
@@ -0,0 +1,41 @@
+import mlflow
+import hashlib
+from mlflow.tracking import MlflowClient
+import pandas as pd
+
+#Instantiate the Client
+client = MlflowClient()
+
+# Get the latest version of model
+model_name = "Logistic Regression"
+latest_versions = client.get_latest_versions(name = model_name, stages = None)
+latest_version = latest_versions[0].version
+print(f"Latest version for model: {model_name} | Version: {latest_version}")
+
+# Get the data hash for that model
+mv = client.get_model_version(
+    name = model_name,
+    version = latest_version
+)
+
+dataset_hash = mv.tags["data_hash"]
+print(f"Data hash for model: {model_name} | Version: {latest_version} | Data Hash: {dataset_hash}")
+
+# Get the run id for that data hash
+experiment_id = "2"
+runs = client.search_runs(
+    experiment_ids=[experiment_id],
+    filter_string=f"tags.data_hash = '{dataset_hash}'"
+)
+
+run_id = runs[0].info.run_id
+print(f"Run id: {run_id}")
+
+# Create a local path for the data artifact of the run
+local_path = client.download_artifacts(
+    run_id = run_id,
+    path = "train_dataset.csv"
+)
+
+df = pd.read_csv(local_path)
+print(df)
